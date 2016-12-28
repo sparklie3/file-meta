9eb109b40cffe0e9e65447cbea7b7e73afa38c96
Shaw <shaw.s.li@gmail.com>
2016-12-28 19:39:03 +0000

    minor updates

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hi');
+var express = require("express");
+var app = express();
+var mongodb = require("mongodb");
+//const mongodb_url = process.env.MONGOLAB_URL;
+//product db collection = "file-meta-db"
+const localMongoUrl = 'mongodb://localhost:27017/data';
+
+
+app.use(express.static('public'));
+app.get('/',function(req,res){
+    //res.send('hi');
+    res.sendFile('index.html',{root:__dirname})
+    res.end();
+})
+
+
+
+
+
+
+app.listen(process.env.PORT, function(){
+    console.log("Server listening on: "+ process.env.PORT);
+});
